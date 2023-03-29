3a511f82b72d5aff4571f76bb743bb0653b155ea
chengbo2023 <123796017+chengbo2023@users.noreply.github.com>
2023-03-29 10:06:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox2222</h1>
       <h2>Start editing to see some magic happen!</h2>
+      console.log("ddd")
     </div>
   );
 }
